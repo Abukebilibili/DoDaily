9ffb0d5748ac6c8fcb54d72feec482dba38b7057
Abuke <huwangpeng@outlook.com>
2024-02-14 23:44:15 +0800

    new

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    // 读取数据库配置
+    config := ReadConfig("config.json")
+    fmt.Println("[info]加载配置:")
+    fmt.Println(config)
+    // 连接数据库
+    db := 
+    fmt.Println("[info]连接数据库:")
+    fmt.Println(db)
 }
